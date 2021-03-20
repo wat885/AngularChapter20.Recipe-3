d77ff1e9830dbdefbbe2e09d2df339beb44a2442
watcharin <twatcharin90@gmail.com>
2021-03-20 11:40:02 +0700

    viewChildren

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core'; import { ProductListComponent } from './product-list/product-list.component'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo2';
+  @ViewChild('productList', { static: true })
+  productList: ProductListComponent;
+ 
+  ngOnInit(): void {
+    this.productList.products = [
+      { name: 'ส้มโอ', price: 111 },
+      { name: 'แตงโม', price: 222 },
+      { name: 'มะพร้าวนํ้าหอม', price: 333 }
+    ]
+
+    
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductItemComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
